50de1c50e1bdf8f963a2a8e5e0247c730714150d
wanmingsong <463693077@qq.com>
2017-02-06 15:13:44 +0800

    first commit

---
+++ app/src/main/java/com/example/test/timeselector/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.test.timeselector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TimeSelector timeSelector;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeSelector = new TimeSelector(this, new TimeSelector.ResultHandler() {
+            @Override
+            public void handle(String time) {
+                Toast.makeText(getApplicationContext(), time, Toast.LENGTH_LONG).show();
+            }
+        }, "1989-01-30 00:00", "2018-12-31 00:00");
+//        timeSelector.setIsLoop(false);
+    }
+    public void show(View v) {
+        timeSelector.show();
+    }
+}
---
+++ app/src/main/java/utils/TextUtil.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * Created by Administrator on 2016/12/29 0029.
+ */
+public class TextUtil {
+
+    public static boolean isEmpty(String str) {
+        if (str == null || "".equals(str) || "null".equalsIgnoreCase(str)) {
+            return true;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
